426174583903a6c5a5b8f2300855c068bf4f64eb
Imanuelm <imanuel.m@indeni.com>
2021-05-10 08:47:41 +0300

    CR-2098: modified unused sg rules tests

--- tests/aws/context_aware/test_ensure_no_unused_security_groups_rule.py
+++ tests/aws/context_aware/test_ensure_no_unused_security_groups_rule.py
@@ -5,6 +5,8 @@ from cloudrail.knowledge.context.aws.ec2.ec2_instance import Ec2Instance
 from cloudrail.knowledge.context.aws.ec2.network_interface import NetworkInterface
 from cloudrail.knowledge.context.aws.ec2.security_group import SecurityGroup
 from cloudrail.knowledge.context.environment_context import EnvironmentContext
+from cloudrail.knowledge.context.terraform_action_type import TerraformActionType
+from cloudrail.knowledge.context.terraform_state import TerraformState
 from cloudrail.knowledge.rules.aws.context_aware.ensure_no_unused_security_groups_rule import EnsureNoUnusedSecurityGroups
 from cloudrail.knowledge.rules.base_rule import RuleResultType
 from cloudrail.dev_tools.rule_test_utils import create_empty_entity
@@ -14,7 +16,7 @@ class TestEnsureNoUnusedSecurityGroups(unittest.TestCase):
     def setUp(self):
         self.rule = EnsureNoUnusedSecurityGroups()
 
-    def test_car_unused_security_group(self):
+    def test_car_unused_security_group_fail(self):
         # Arrange
         security_group_1: SecurityGroup = create_empty_entity(SecurityGroup)
         security_group_2: SecurityGroup = create_empty_entity(SecurityGroup)
@@ -23,6 +25,10 @@ class TestEnsureNoUnusedSecurityGroups(unittest.TestCase):
         network_interface.security_groups.append(security_group_1)
         network_interface.owner = ec2
         ec2.network_resource.network_interfaces.append(network_interface)
+        security_group_2.terraform_state = TerraformState(address='address',
+                                                          action=TerraformActionType.NO_OP,
+                                                          resource_metadata=None,
+                                                          is_new=False)
         context = EnvironmentContext(ec2s=[ec2], network_interfaces=AliasesDict(network_interface),
                                      security_groups=AliasesDict(security_group_1, security_group_2))
         # Act
@@ -31,7 +37,7 @@ class TestEnsureNoUnusedSecurityGroups(unittest.TestCase):
         self.assertEqual(RuleResultType.FAILED, result.status)
         self.assertEqual(1, len(result.issues))
 
-    def test_car_iam_policy_control_in_iac_only_pass(self):
+    def test_car_unused_security_group__no_unused_sg__pass(self):
         # Arrange
         security_group_1: SecurityGroup = create_empty_entity(SecurityGroup)
         ec2: Ec2Instance = create_empty_entity(Ec2Instance)
@@ -41,6 +47,27 @@ class TestEnsureNoUnusedSecurityGroups(unittest.TestCase):
         ec2.network_resource.network_interfaces.append(network_interface)
         context = EnvironmentContext(ec2s=[ec2], network_interfaces=AliasesDict(network_interface),
                                      security_groups=AliasesDict(security_group_1))
+        # Act
+        result = self.rule.run(context, {})
+        # Assert
+        self.assertEqual(RuleResultType.SUCCESS, result.status)
+        self.assertEqual(0, len(result.issues))
+
+    def test_car_unused_security_group__unused_sg_is_new__pass(self):
+        # Arrange
+        security_group_1: SecurityGroup = create_empty_entity(SecurityGroup)
+        security_group_2: SecurityGroup = create_empty_entity(SecurityGroup)
+        ec2: Ec2Instance = create_empty_entity(Ec2Instance)
+        network_interface: NetworkInterface = create_empty_entity(NetworkInterface)
+        network_interface.security_groups.append(security_group_1)
+        network_interface.owner = ec2
+        ec2.network_resource.network_interfaces.append(network_interface)
+        security_group_2.terraform_state = TerraformState(address='address',
+                                                          action=TerraformActionType.CREATE,
+                                                          resource_metadata=None,
+                                                          is_new=True)
+        context = EnvironmentContext(ec2s=[ec2], network_interfaces=AliasesDict(network_interface),
+                                     security_groups=AliasesDict(security_group_1, security_group_2))
         # Act
         result = self.rule.run(context, {})
         # Assert
