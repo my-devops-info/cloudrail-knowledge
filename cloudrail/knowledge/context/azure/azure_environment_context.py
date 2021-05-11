206f06934ae28c5e410673ae0cab96c44a9d5037
Tomer Vainshtok <tomerv@indeni.com>
2021-05-11 16:10:27 +0300

    Added checkov_results to azure context

--- cloudrail/knowledge/context/azure/azure_environment_context.py
+++ cloudrail/knowledge/context/azure/azure_environment_context.py
@@ -1,12 +1,20 @@
+from typing import List, Dict
+
 from cloudrail.knowledge.context.aliases_dict import AliasesDict
 from cloudrail.knowledge.context.azure.azure_resources.azure_resource_group import AzureResourceGroup
 from cloudrail.knowledge.context.azure.azure_resources.databases.azure_sql_server import AzureSqlServer
 from cloudrail.knowledge.context.base_environment_context import BaseEnvironmentContext
+from cloudrail.knowledge.context.environment_context import CheckovResult
 
 
 class AzureEnvironmentContext(BaseEnvironmentContext):
 
-    def __init__(self) -> None:
+    def __init__(self,
+                 checkov_results: Dict[str, List[CheckovResult]] = None,
+                 resource_groups: AliasesDict[AzureResourceGroup] = None,
+                 sql_servers: AliasesDict[AzureSqlServer] = None) -> None:
         BaseEnvironmentContext.__init__(self)
-        self.resource_groups: AliasesDict[AzureResourceGroup] = AliasesDict()
-        self.sql_servers: AliasesDict[AzureSqlServer] = AliasesDict()
+        self.checkov_results: Dict[str, List[CheckovResult]] = checkov_results or {}
+        self.resource_groups: AliasesDict[AzureResourceGroup] = resource_groups or AliasesDict()
+        self.sql_servers: AliasesDict[AzureSqlServer] = sql_servers or AliasesDict()
+
