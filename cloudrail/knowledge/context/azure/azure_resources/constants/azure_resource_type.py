728d06f617970641900268f4da8b7ff15da473e9
Tomer Vainshtok <tomerv@indeni.com>
2021-05-12 11:52:26 +0300

    Added minimal AppService model for checkov results

--- cloudrail/knowledge/context/azure/azure_resources/constants/azure_resource_type.py
+++ cloudrail/knowledge/context/azure/azure_resources/constants/azure_resource_type.py
@@ -7,3 +7,4 @@ class AzureResourceType(Enum):
     AZURERM_SQL_SERVER = 'azurerm_sql_server'
     AZURERM_MSSQL_SERVER = 'azurerm_mssql_server'
     AZURERM_SQL_FIREWALL_RULE = 'azurerm_sql_firewall_rule'
+    AZURERM_APP_SERVICE = 'azurerm_app_service'
--- cloudrail/knowledge/context/azure/azure_resources/databases/azure_app_service.py
+++ cloudrail/knowledge/context/azure/azure_resources/databases/azure_app_service.py
@@ -3,21 +3,17 @@ from cloudrail.knowledge.context.azure.azure_resources.azure_resource import Azu
 from cloudrail.knowledge.context.azure.azure_resources.constants.azure_resource_type import AzureResourceType
 
 
-class AzureSqlServer(AzureResource):
-
-    def __init__(self, subscription_id: str, resource_group_name: str, location: str,
-                 server_name: str, public_network_access_enable: bool) -> None:
+class AzureAppService(AzureResource):
+    def __init__(self, subscription_id: str, resource_group_name: str, location: str, name: str) -> None:
         super().__init__(subscription_id, resource_group_name, location,
-                         'Microsoft.Sql/servers', AzureResourceType.AZURERM_SQL_SERVER)
-        self.server_name: str = server_name
-        self.with_aliases(server_name)
-        self.public_network_access_enable: bool = public_network_access_enable
+                         '', AzureResourceType.AZURERM_APP_SERVICE)
+        self.name = name
 
     def get_keys(self) -> List[str]:
         return [self.get_name()]
 
     def get_name(self) -> str:
-        return self.server_name
+        return self.name
 
     def get_cloud_resource_url(self) -> Optional[str]:
         pass
