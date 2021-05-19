6b26db2931749b9478a228b5c9f4738d88bca21d
Imanuelm <imanuel.m@indeni.com>
2021-05-19 09:04:27 +0300

    CR-2100: revert context aware rules tests

--- tests/aws/context_aware/indirect_public_access_rules/test_s3_bucket_lambda_indirect_exposure_rule.py
+++ tests/aws/context_aware/indirect_public_access_rules/test_s3_bucket_lambda_indirect_exposure_rule.py
@@ -1,20 +1,6 @@
 # import unittest
 #
-# from cloudrail.knowledge.context.aliases_dict import AliasesDict
-# from cloudrail.knowledge.context.aws.apigateway.api_gateway_integration import ApiGatewayIntegration
-# from cloudrail.knowledge.context.aws.apigateway.api_gateway_method import ApiGatewayMethod
-# from cloudrail.knowledge.context.aws.apigateway.api_gateway_method_settings import ApiGatewayMethodSettings, RestApiMethods
-# from cloudrail.knowledge.context.aws.apigateway.rest_api_gw import RestApiGw
-# from cloudrail.knowledge.context.aws.aws_connection import PolicyEvaluation
-# from cloudrail.knowledge.context.aws.iam.policy import AssumeRolePolicy, Policy, S3Policy
-# from cloudrail.knowledge.context.aws.iam.policy_statement import PolicyStatement, StatementCondition, StatementEffect
-# from cloudrail.knowledge.context.aws.iam.principal import Principal, PrincipalType
-# from cloudrail.knowledge.context.aws.iam.role import Role
-# from cloudrail.knowledge.context.aws.lambda_.lambda_function import LambdaFunction
-# from cloudrail.knowledge.context.aws.s3.s3_bucket import S3Bucket
 # from cloudrail.knowledge.context.environment_context import EnvironmentContext
-# from cloudrail.knowledge.rules.aws.context_aware.indirect_public_access_rules.s3_bucket_lambda_indirect_exposure_rule import \
-#     S3BucketLambdaIndirectExposureRule
 # from cloudrail.knowledge.rules.base_rule import RuleResultType
 # from cloudrail.dev_tools.rule_test_utils import create_empty_entity
 #
@@ -25,40 +11,7 @@
 #
 #     def test_s3_lambda_indirect_exposure_fail(self):
 #         # Arrange
-#         s3_bucket = S3Bucket('111111', 's3_bucket_name', 's3_bucket_arn')
-#         rest_api_gw: RestApiGw = create_empty_entity(RestApiGw)
-#         api_gw_method: ApiGatewayMethod = create_empty_entity(ApiGatewayMethod)
-#         method_settings: ApiGatewayMethodSettings = create_empty_entity(ApiGatewayMethodSettings)
-#         gw_integration: ApiGatewayIntegration = create_empty_entity(ApiGatewayIntegration)
-#         role: Role = create_empty_entity(Role)
-#         policy_eval: PolicyEvaluation = create_empty_entity(PolicyEvaluation)
-#         policy_condition = [StatementCondition(operator='BOOL', key='aws:SecureTransport', values=['false'])]
-#         s3_bucket.resource_based_policy = S3Policy('account', 'bucket_name', [PolicyStatement(StatementEffect.DENY, ['s3:*'],
-#                                                                                               ['*'], Principal(PrincipalType.PUBLIC, ['*']),
-#                                                                                               'statement_id', policy_condition)],
-#                                                    'raw_doc')
-#         lambda_func: LambdaFunction = create_empty_entity(LambdaFunction)
-#         lambda_func.resource_based_policy = Policy('account', [PolicyStatement(StatementEffect.ALLOW,
-#                                                                                ['lambda:*'], ['*'], Principal(PrincipalType.PUBLIC, ['*']))])
-#         assume_role_policy = AssumeRolePolicy('111111111', 'test-role', 'arn:aws:iam::123456789:role/test-role',
-#                                               [PolicyStatement(StatementEffect.ALLOW,
-#                                                                ['sts:AssumeRole'],
-#                                                                ['*'],
-#                                                                Principal(PrincipalType.PUBLIC, ['*']))], 'state_id')
-#         role.assume_role_policy = assume_role_policy
-#         policy_eval.resource_denied_actions = {'s3:PutObject'}
-#         policy_eval.resource_allowed_actions = set()
-#         role.policy_evaluation_result_map = {'something', policy_eval}
-#         method_settings.http_method = RestApiMethods.HEAD
-#         method_settings.stage_name = 'method_tests'
-#         rest_api_gw.method_settings = method_settings
-#         rest_api_gw.rest_api_gw_id = 'rest_api_id'
-#         api_gw_method.rest_api_id = 'rest_api_id'
-#         gw_integration.lambda_func_integration = lambda_func
-#         gw_integration.lambda_func_integration.iam_role = role
-#         api_gw_method.integration = gw_integration
-#         context = EnvironmentContext(s3_buckets=AliasesDict(*[s3_bucket]), lambda_function_list=[lambda_func], rest_api_gw=[rest_api_gw],
-#                                      api_gateway_methods=[api_gw_method])
+#         context = EnvironmentContext()
 #         # Act
 #         result = self.rule.run(context, {})
 #         # Assert
--- tests/aws/context_aware/public_access_validation_rules/test_indirect_public_access_db_rds_rule.py
+++ tests/aws/context_aware/public_access_validation_rules/test_indirect_public_access_db_rds_rule.py
@@ -1,18 +1,6 @@
 # import unittest
 #
-# from cloudrail.knowledge.context.aws.aws_connection import ConnectionDirectionType, ConnectionInstance, PolicyConnectionProperty, \
-#     PortConnectionProperty, PrivateConnectionDetail, \
-#     PublicConnectionDetail
-# from cloudrail.knowledge.context.aws.ec2.network_acl import NetworkAcl
-# from cloudrail.knowledge.context.aws.ec2.network_interface import NetworkInterface
-# from cloudrail.knowledge.context.aws.ec2.security_group import SecurityGroup
-# from cloudrail.knowledge.context.aws.ec2.security_group_rule import ConnectionType, SecurityGroupRule, SecurityGroupRulePropertyType
-# from cloudrail.knowledge.context.aws.ec2.subnet import Subnet
-# from cloudrail.knowledge.context.aws.networking_config.network_resource import NetworkResource
-# from cloudrail.knowledge.context.aws.rds.rds_cluster import RdsCluster
-# from cloudrail.knowledge.context.aws.rds.rds_instance import RdsInstance
 # from cloudrail.knowledge.context.environment_context import EnvironmentContext
-# from cloudrail.knowledge.rules.aws.context_aware.indirect_public_access_rules.indirect_public_access_db_rds_rule import IndirectPublicAccessDbRds
 # from cloudrail.knowledge.rules.base_rule import RuleResultType
 # from cloudrail.dev_tools.rule_test_utils import create_empty_entity
 #
@@ -21,186 +9,18 @@
 #     def setUp(self):
 #         self.rule = IndirectPublicAccessDbRds()
 #
-#     def test_indirect_public_access_db_rds__rds_cluster__fail(self):
+#     def test_indirect_public_access_db_rds_fail(self):
 #         # Arrange
-#         rds_cluster: RdsCluster = create_empty_entity(RdsCluster)
-#         network_resource: NetworkResource = create_empty_entity(NetworkResource)
-#         network_interface = create_empty_entity(NetworkInterface)
-#         security_group: SecurityGroup = create_empty_entity(SecurityGroup)
-#         rds_instance: RdsInstance = create_empty_entity(RdsInstance)
-#         subnet: Subnet = create_empty_entity(Subnet)
-#         network_acl: NetworkAcl = create_empty_entity(NetworkAcl)
-#         network_acl.name = 'subnet_network_acl_name'
-#         subnet.name = 'subnet_testing_name'
-#         subnet.subnet_id = 'subnet_testing_id'
-#         subnet.network_acl = network_acl
-#         inbound_permissions = [SecurityGroupRule(0, 65535, '-1', SecurityGroupRulePropertyType.IP_RANGES,
-#                                                  '0.0.0.0/0', False, ConnectionType.Inbound,
-#                                                  'AllInclusiveSecurityGroup', 'us-east-1', '1111111')]
-#         outbound_permissions = [SecurityGroupRule(0, 65535, '-1',
-#                                                   SecurityGroupRulePropertyType.IP_RANGES, '10.10.10.0/24', False,
-#                                                   ConnectionType.Outbound, 'AllInclusiveSecurityGroup', 'us-east-1', '1111111')]
-#         security_group.security_group_id = 'sg-pseudo-some-id'
-#         security_group.inbound_permissions = inbound_permissions
-#         security_group.outbound_permissions = outbound_permissions
-#         network_interface.security_groups.append(security_group)
-#         connection_detail = PublicConnectionDetail(PolicyConnectionProperty([]), ConnectionDirectionType.INBOUND)
-#         network_interface.inbound_connections.add(connection_detail)
-#         network_resource.network_interfaces.append(network_interface)
-#         rds_instance.publicly_accessible = True
-#         rds_instance.security_group_ids = [security_group]
-#         rds_instance.name = 'rds_instance_name'
-#         rds_instance.network_resource = network_resource
-#         rds_instance.network_resource.network_interfaces[0].add_private_inbound_conn(PortConnectionProperty(ports=[(443, 443)],
-#                                                                                                             cidr_block='0.0.0.0/0',
-#                                                                                                             ip_protocol_type='tcp'),
-#                                                                                      rds_instance.network_resource.network_interfaces[0])
-#         rds_instance.network_resource.network_interfaces[0].secondary_ip_addresses = ['10.10.10.16/24']
-#         rds_instance.network_resource.network_interfaces[0].primary_ip_address = '10.10.10.10/24'
-#         rds_instance.network_resource.network_interfaces[0].owner = rds_instance
-#         rds_instance.network_resource.network_interfaces[0].subnet = subnet
-#         rds_instance.port = 3306
-#         rds_cluster.cluster_instances = [rds_instance]
-#         rds_cluster.cluster_id = 'rds_cluster_id'
-#         context = EnvironmentContext(rds_clusters=[rds_cluster])
+#         context = EnvironmentContext()
 #         # Act
 #         result = self.rule.run(context, {})
 #         # Assert
 #         self.assertEqual(RuleResultType.FAILED, result.status)
 #         self.assertEqual(1, len(result.issues))
 #
-#     def test_indirect_public_access_db_rds__rds_instance__fail(self):
+#     def test_indirect_public_access_db_rds_pass(self):
 #         # Arrange
-#         network_resource: NetworkResource = create_empty_entity(NetworkResource)
-#         network_interface = create_empty_entity(NetworkInterface)
-#         security_group: SecurityGroup = create_empty_entity(SecurityGroup)
-#         rds_instance: RdsInstance = create_empty_entity(RdsInstance)
-#         subnet: Subnet = create_empty_entity(Subnet)
-#         network_acl: NetworkAcl = create_empty_entity(NetworkAcl)
-#         network_acl.name = 'subnet_network_acl_name'
-#         subnet.name = 'subnet_testing_name'
-#         subnet.subnet_id = 'subnet_testing_id'
-#         subnet.network_acl = network_acl
-#         inbound_permissions = [SecurityGroupRule(0, 65535, '-1', SecurityGroupRulePropertyType.IP_RANGES,
-#                                                  '0.0.0.0/0', False, ConnectionType.Inbound,
-#                                                  'AllInclusiveSecurityGroup', 'us-east-1', '1111111')]
-#         outbound_permissions = [SecurityGroupRule(0, 65535, '-1',
-#                                                   SecurityGroupRulePropertyType.IP_RANGES, '10.10.10.0/24', False,
-#                                                   ConnectionType.Outbound, 'AllInclusiveSecurityGroup', 'us-east-1', '1111111')]
-#         security_group.security_group_id = 'sg-pseudo-some-id'
-#         security_group.inbound_permissions = inbound_permissions
-#         security_group.outbound_permissions = outbound_permissions
-#         network_interface.security_groups.append(security_group)
-#         connection_detail = PublicConnectionDetail(PolicyConnectionProperty([]), ConnectionDirectionType.INBOUND)
-#         network_interface.inbound_connections.add(connection_detail)
-#         network_resource.network_interfaces.append(network_interface)
-#         rds_instance.publicly_accessible = True
-#         rds_instance.security_group_ids = [security_group]
-#         rds_instance.name = 'rds_instance_name'
-#         rds_instance.network_resource = network_resource
-#         rds_instance.network_resource.network_interfaces[0].add_private_inbound_conn(PortConnectionProperty(ports=[(443, 443)],
-#                                                                                                             cidr_block='0.0.0.0/0',
-#                                                                                                             ip_protocol_type='tcp'),
-#                                                                                      rds_instance.network_resource.network_interfaces[0])
-#         rds_instance.network_resource.network_interfaces[0].secondary_ip_addresses = ['10.10.10.16/24']
-#         rds_instance.network_resource.network_interfaces[0].primary_ip_address = '10.10.10.10/24'
-#         rds_instance.network_resource.network_interfaces[0].owner = rds_instance
-#         rds_instance.network_resource.network_interfaces[0].subnet = subnet
-#         rds_instance.port = 3306
-#         context = EnvironmentContext(rds_instances=[rds_instance])
-#         # Act
-#         result = self.rule.run(context, {})
-#         # Assert
-#         self.assertEqual(RuleResultType.FAILED, result.status)
-#         self.assertEqual(1, len(result.issues))
-#
-#     def test_indirect_public_access_db_rds__rds_cluster__pass(self):
-#         # Arrange
-#         rds_cluster: RdsCluster = create_empty_entity(RdsCluster)
-#         network_resource: NetworkResource = create_empty_entity(NetworkResource)
-#         network_interface = create_empty_entity(NetworkInterface)
-#         security_group: SecurityGroup = create_empty_entity(SecurityGroup)
-#         rds_instance: RdsInstance = create_empty_entity(RdsInstance)
-#         subnet: Subnet = create_empty_entity(Subnet)
-#         network_acl: NetworkAcl = create_empty_entity(NetworkAcl)
-#         network_acl.name = 'subnet_network_acl_name'
-#         subnet.name = 'subnet_testing_name'
-#         subnet.subnet_id = 'subnet_testing_id'
-#         subnet.network_acl = network_acl
-#         inbound_permissions = [SecurityGroupRule(0, 65535, '-1', SecurityGroupRulePropertyType.IP_RANGES,
-#                                                  '0.0.0.0/0', False, ConnectionType.Inbound,
-#                                                  'AllInclusiveSecurityGroup', 'us-east-1', '1111111')]
-#         outbound_permissions = [SecurityGroupRule(0, 65535, '-1',
-#                                                   SecurityGroupRulePropertyType.IP_RANGES, '10.10.10.0/24', False,
-#                                                   ConnectionType.Outbound, 'AllInclusiveSecurityGroup', 'us-east-1', '1111111')]
-#         security_group.security_group_id = 'sg-pseudo-some-id'
-#         security_group.inbound_permissions = inbound_permissions
-#         security_group.outbound_permissions = outbound_permissions
-#         network_interface.security_groups.append(security_group)
-#         connection_detail = PrivateConnectionDetail(PolicyConnectionProperty([]), ConnectionDirectionType.INBOUND, ConnectionInstance())
-#         network_interface.inbound_connections.add(connection_detail)
-#         network_resource.network_interfaces.append(network_interface)
-#         rds_instance.publicly_accessible = True
-#         rds_instance.security_group_ids = [security_group]
-#         rds_instance.name = 'rds_instance_name'
-#         rds_instance.network_resource = network_resource
-#         rds_instance.network_resource.network_interfaces[0].add_private_inbound_conn(PortConnectionProperty(ports=[(443, 443)],
-#                                                                                                             cidr_block='0.0.0.0/0',
-#                                                                                                             ip_protocol_type='tcp'),
-#                                                                                      rds_instance.network_resource.network_interfaces[0])
-#         rds_instance.network_resource.network_interfaces[0].secondary_ip_addresses = ['10.10.10.16/24']
-#         rds_instance.network_resource.network_interfaces[0].primary_ip_address = '10.10.10.10/24'
-#         rds_instance.network_resource.network_interfaces[0].owner = rds_instance
-#         rds_instance.network_resource.network_interfaces[0].subnet = subnet
-#         rds_instance.port = 3306
-#         rds_cluster.cluster_instances = [rds_instance]
-#         rds_cluster.cluster_id = 'rds_cluster_id'
-#         context = EnvironmentContext(rds_clusters=[rds_cluster])
-#         # Act
-#         result = self.rule.run(context, {})
-#         # Assert
-#         self.assertEqual(RuleResultType.SUCCESS, result.status)
-#         self.assertEqual(0, len(result.issues))
-#
-#     def test_indirect_public_access_db_rds__rds_instance__pass(self):
-#         # Arrange
-#         network_resource: NetworkResource = create_empty_entity(NetworkResource)
-#         network_interface = create_empty_entity(NetworkInterface)
-#         security_group: SecurityGroup = create_empty_entity(SecurityGroup)
-#         rds_instance: RdsInstance = create_empty_entity(RdsInstance)
-#         subnet: Subnet = create_empty_entity(Subnet)
-#         network_acl: NetworkAcl = create_empty_entity(NetworkAcl)
-#         network_acl.name = 'subnet_network_acl_name'
-#         subnet.name = 'subnet_testing_name'
-#         subnet.subnet_id = 'subnet_testing_id'
-#         subnet.network_acl = network_acl
-#         inbound_permissions = [SecurityGroupRule(0, 65535, '-1', SecurityGroupRulePropertyType.IP_RANGES,
-#                                                  '0.0.0.0/0', False, ConnectionType.Inbound,
-#                                                  'AllInclusiveSecurityGroup', 'us-east-1', '1111111')]
-#         outbound_permissions = [SecurityGroupRule(0, 65535, '-1',
-#                                                   SecurityGroupRulePropertyType.IP_RANGES, '10.10.10.0/24', False,
-#                                                   ConnectionType.Outbound, 'AllInclusiveSecurityGroup', 'us-east-1', '1111111')]
-#         security_group.security_group_id = 'sg-pseudo-some-id'
-#         security_group.inbound_permissions = inbound_permissions
-#         security_group.outbound_permissions = outbound_permissions
-#         network_interface.security_groups.append(security_group)
-#         connection_detail = PrivateConnectionDetail(PolicyConnectionProperty([]), ConnectionDirectionType.INBOUND, ConnectionInstance())
-#         network_interface.inbound_connections.add(connection_detail)
-#         network_resource.network_interfaces.append(network_interface)
-#         rds_instance.publicly_accessible = True
-#         rds_instance.security_group_ids = [security_group]
-#         rds_instance.name = 'rds_instance_name'
-#         rds_instance.network_resource = network_resource
-#         rds_instance.network_resource.network_interfaces[0].add_private_inbound_conn(PortConnectionProperty(ports=[(443, 443)],
-#                                                                                                             cidr_block='0.0.0.0/0',
-#                                                                                                             ip_protocol_type='tcp'),
-#                                                                                      rds_instance.network_resource.network_interfaces[0])
-#         rds_instance.network_resource.network_interfaces[0].secondary_ip_addresses = ['10.10.10.16/24']
-#         rds_instance.network_resource.network_interfaces[0].primary_ip_address = '10.10.10.10/24'
-#         rds_instance.network_resource.network_interfaces[0].owner = rds_instance
-#         rds_instance.network_resource.network_interfaces[0].subnet = subnet
-#         rds_instance.port = 3306
-#         context = EnvironmentContext(rds_instances=[rds_instance])
+#         context = EnvironmentContext()
 #         # Act
 #         result = self.rule.run(context, {})
 #         # Assert
