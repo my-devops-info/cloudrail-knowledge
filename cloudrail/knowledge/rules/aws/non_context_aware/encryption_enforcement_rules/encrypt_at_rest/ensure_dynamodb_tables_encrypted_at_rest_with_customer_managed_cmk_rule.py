3bd06789aa869349a2dd241726932f6112325979
Imanuelm <imanuel.m@indeni.com>
2021-05-23 17:57:33 +0300

    CR-1435: modified builders and added rule

---
+++ cloudrail/knowledge/rules/aws/non_context_aware/encryption_enforcement_rules/encrypt_at_rest/ensure_dynamodb_tables_encrypted_at_rest_with_customer_managed_cmk_rule.py
@@ -0,0 +1,27 @@
+from typing import List, Dict
+from cloudrail.knowledge.context.aws.kms.kms_key_manager import KeyManager
+
+from cloudrail.knowledge.context.environment_context import EnvironmentContext
+from cloudrail.knowledge.rules.aws.aws_base_rule import AwsBaseRule
+from cloudrail.knowledge.rules.base_rule import Issue
+from cloudrail.knowledge.rules.rule_parameters.base_paramerter import ParameterType
+
+
+class EnsureDynamoDbTableEncryptedAtRestWithCustomerManagedCmkRule(AwsBaseRule):
+
+    def get_id(self) -> str:
+        return 'non_car_dynamodb_tables_encrypted_at_rest_with_customer_managed_CMK'
+
+    def execute(self, env_context: EnvironmentContext, parameters: Dict[ParameterType, any]) -> List[Issue]:
+        issues: List[Issue] = []
+
+        for table in env_context.dynamodb_table_list:
+            if not table.server_side_encryption or (table.server_side_encryption and table.kms_data.key_manager != KeyManager.CUSTOMER):
+                issues.append(
+                    Issue(
+                        f'The {table.get_type()} `{table.get_friendly_name()}` '
+                        f'is not set to be encrypted at rest using customer-managed CMK', table, table))
+        return issues
+
+    def should_run_rule(self, environment_context: EnvironmentContext) -> bool:
+        return bool(environment_context.dynamodb_table_list)
