05ba682d08333c753796043605ec01f58b706ef4
Imanuelm <imanuel.m@indeni.com>
2021-05-24 12:29:10 +0300

    CR-1479: rule ready with tests

---
+++ cloudrail/knowledge/rules/aws/non_context_aware/encryption_enforcement_rules/encrypt_at_rest/ensure_ecr_repositories_encrypt_at_rest_with_customer_cmk_rule.py
@@ -0,0 +1,28 @@
+from typing import List, Dict
+from cloudrail.knowledge.context.aws.kms.kms_key_manager import KeyManager
+
+from cloudrail.knowledge.context.environment_context import EnvironmentContext
+from cloudrail.knowledge.rules.aws.aws_base_rule import AwsBaseRule
+from cloudrail.knowledge.rules.base_rule import Issue
+from cloudrail.knowledge.rules.rule_parameters.base_paramerter import ParameterType
+
+
+class EnsureEcrRepositoriesEncryptedAtRestWithCustomerManagedCmkRule(AwsBaseRule):
+
+    def get_id(self) -> str:
+        return 'non_car_ecr_repositories_encrypted_at_rest_with_customer_managed_cmk'
+
+    def execute(self, env_context: EnvironmentContext, parameters: Dict[ParameterType, any]) -> List[Issue]:
+        issues: List[Issue] = []
+
+        for ecr in env_context.ecr_repositories:
+            if ecr.is_new_resource():
+                if ecr.encryption_type != 'KMS' or (ecr.encryption_type == 'KMS' and ecr.kms_data.key_manager != KeyManager.CUSTOMER):
+                    issues.append(
+                        Issue(
+                            f'The {ecr.get_type()} `{ecr.get_friendly_name()}` '
+                            f'is not set to be encrypted at rest using customer-managed CMK', ecr, ecr))
+        return issues
+
+    def should_run_rule(self, environment_context: EnvironmentContext) -> bool:
+        return bool(environment_context.ecr_repositories)
