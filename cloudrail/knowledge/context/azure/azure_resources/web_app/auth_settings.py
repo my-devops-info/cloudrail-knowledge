abd38003ae956b51a3ff9909f1eafa9e44476f0f
Idan Aviv <idana@indeni.com>
2021-06-03 17:24:31 +0300

    FunctionAppClientCertificateModeRule: new azure rule

--- cloudrail/knowledge/context/azure/azure_resources/web_app/auth_settings.py
+++ cloudrail/knowledge/context/azure/azure_resources/web_app/auth_settings.py
@@ -1,4 +1,8 @@
+from cloudrail.knowledge.context.azure.azure_resources.web_app.constants import FieldMode
+
+
 class AuthSettings:
 
-    def __init__(self, enabled: bool) -> None:
+    def __init__(self, enabled: bool, client_cert_mode: FieldMode) -> None:
         self.enabled: bool = enabled
+        self.client_cert_mode: FieldMode = client_cert_mode
--- tests/knowledge/rules/azure/non_context_aware/test_function_app_client_certificate_mode.py
+++ tests/knowledge/rules/azure/non_context_aware/test_function_app_client_certificate_mode.py
@@ -1,22 +1,22 @@
 from unittest import TestCase
-
 from cloudrail.dev_tools.rule_test_utils import create_empty_entity
 from cloudrail.knowledge.context.aliases_dict import AliasesDict
 from cloudrail.knowledge.context.azure.azure_environment_context import AzureEnvironmentContext
 from cloudrail.knowledge.context.azure.azure_resources.web_app.auth_settings import AuthSettings
 from cloudrail.knowledge.context.azure.azure_resources.web_app.azure_function_app import AzureFunctionApp
-from cloudrail.knowledge.rules.azure.non_context_aware.function_app_authentication_enable_rule import FunctionAppAuthenticationEnableRule
+from cloudrail.knowledge.context.azure.azure_resources.web_app.constants import FieldMode
+from cloudrail.knowledge.rules.azure.non_context_aware.function_app_client_certificate_mode_rule import FunctionAppClientCertificateModeRule
 from cloudrail.knowledge.rules.base_rule import RuleResultType
 
 
-class TestFunctionAppAuthenticationEnable(TestCase):
+class TestFunctionAppClientCertificateMode(TestCase):
 
     def setUp(self):
-        self.rule = FunctionAppAuthenticationEnableRule()
+        self.rule = FunctionAppClientCertificateModeRule()
 
-    def test_ftps_enabled(self):
+    def test_client_cert_required(self):
         # Arrange
-        auth_settings: AuthSettings = AuthSettings(True, False)
+        auth_settings: AuthSettings = AuthSettings(False, FieldMode.REQUIRED)
         func_app: AzureFunctionApp = create_empty_entity(AzureFunctionApp)
         func_app.auth_settings = auth_settings
         func_app.name = 'my-func-app'
@@ -27,9 +27,9 @@ class TestFunctionAppAuthenticationEnable(TestCase):
         self.assertEqual(RuleResultType.SUCCESS, result.status)
         self.assertEqual(0, len(result.issues))
 
-    def test_ftps_disabled(self):
+    def test_client_cert_optional(self):
         # Arrange
-        auth_settings: AuthSettings = AuthSettings(False, False)
+        auth_settings: AuthSettings = AuthSettings(False, FieldMode.OPTIONAL)
         func_app: AzureFunctionApp = create_empty_entity(AzureFunctionApp)
         func_app.auth_settings = auth_settings
         func_app.name = 'my-func-app'
