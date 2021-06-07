16cc6a1809d5e61730043aae1d89f6fbdea884fb
Idan Aviv <idana@indeni.com>
2021-06-07 11:56:56 +0300

    import fix

--- cloudrail/knowledge/rules/azure/non_context_aware/function_app_client_certificate_mode_rule.py
+++ cloudrail/knowledge/rules/azure/non_context_aware/function_app_client_certificate_mode_rule.py
@@ -1,6 +1,6 @@
 from typing import List, Dict
 from cloudrail.knowledge.context.azure.azure_environment_context import AzureEnvironmentContext
-from cloudrail.knowledge.context.azure.azure_resources.web_app.constants import FieldMode
+from cloudrail.knowledge.context.azure.webapp.constants import FieldMode
 from cloudrail.knowledge.rules.azure.azure_base_rule import AzureBaseRule
 from cloudrail.knowledge.rules.base_rule import Issue
 from cloudrail.knowledge.rules.rule_parameters.base_paramerter import ParameterType
