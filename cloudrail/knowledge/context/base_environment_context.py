7a509c4893f9c17d4cf2d56ebd0229cfeddc2eca
IdoRadai <>
2021-06-08 08:58:31 +0300

    move checkov results to base env

--- cloudrail/knowledge/context/base_environment_context.py
+++ cloudrail/knowledge/context/base_environment_context.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from typing import Set, List
 
 from cloudrail.knowledge.context.managed_resources_summary import ManagedResourcesSummary
@@ -23,3 +24,16 @@ class BaseEnvironmentContext:
                     func.cache_clear()  # clearing lru_cache
                 except Exception:
                     pass
+
+
+@dataclass
+class CheckovResult:
+    check_id: str
+    file_path: str
+    resource: str
+    start_line: int
+    end_line: int
+
+    @staticmethod
+    def from_dict(dic: dict):
+        return CheckovResult(dic['check_id'], dic['file_path'], dic['resource'], dic['start_line'], dic['end_line'])
