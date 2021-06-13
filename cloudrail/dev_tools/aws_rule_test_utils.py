8d0b803b77a83a28e2c6e21ea18afd085ff85dd4
Tomer Vainshtok <tomerv@indeni.com>
2021-06-13 18:15:27 +0300

    Added tests for PublicAccessEksApiRule

--- cloudrail/dev_tools/aws_rule_test_utils.py
+++ cloudrail/dev_tools/aws_rule_test_utils.py
@@ -3,6 +3,7 @@ from typing import Optional, TypeVar, Type
 from cloudrail.dev_tools.rule_test_utils import create_empty_entity
 from cloudrail.knowledge.context.aws.ec2.network_acl import NetworkAcl
 from cloudrail.knowledge.context.aws.ec2.network_interface import NetworkInterface
+from cloudrail.knowledge.context.aws.ec2.route_table import RouteTable
 from cloudrail.knowledge.context.aws.ec2.subnet import Subnet
 from cloudrail.knowledge.context.aws.ec2.vpc import Vpc
 from cloudrail.knowledge.context.aws.networking_config.network_entity import NetworkEntity
@@ -11,7 +12,7 @@ from cloudrail.knowledge.context.aws.networking_config.network_entity import Net
 _T = TypeVar('_T', bound=NetworkEntity)
 
 
-def create_network_entity(_type: Type[_T], subnet: Optional[Subnet] = None) -> _T:
+def create_empty_network_entity(_type: Type[_T], subnet: Optional[Subnet] = None) -> _T:
     instance: NetworkEntity = create_empty_entity(_type)
     if not subnet:
         subnet = create_empty_entity(Subnet)
@@ -23,6 +24,9 @@ def create_network_entity(_type: Type[_T], subnet: Optional[Subnet] = None) -> _
         nacl = create_empty_entity(NetworkAcl)
         nacl.name = 'network-acl-name'
         subnet.network_acl = nacl
+    if not subnet.route_table:
+        subnet.route_table = create_empty_entity(RouteTable)
+        subnet.route_table.name = 'route-table-name'
     eni = create_empty_entity(NetworkInterface)
     eni.owner = instance
     eni.subnet = subnet
