31d2e2311f20f724488f158e59eddd50b8fe8363
Idan Aviv <idana@indeni.com>
2021-06-14 17:12:28 +0300

    uncommen FunctionAppEnforcesFtpsOnlyRule

--- cloudrail/knowledge/context/azure/webapp/azure_function_app.py
+++ cloudrail/knowledge/context/azure/webapp/azure_function_app.py
@@ -4,6 +4,7 @@ from cloudrail.knowledge.context.azure.azure_resource import AzureResource
 from cloudrail.knowledge.context.azure.webapp.auth_settings import AuthSettings
 from cloudrail.knowledge.context.azure.constants.azure_resource_type import AzureResourceType
 from cloudrail.knowledge.context.azure.webapp.constants import FieldMode
+from cloudrail.knowledge.context.azure.webapp.site_config import SiteConfig
 
 
 class AzureFunctionApp(AzureResource):
@@ -11,11 +12,13 @@ class AzureFunctionApp(AzureResource):
         Attributes:
             name: Function app resource name.
             auth_settings: Function app authentication settings.
+            site_config: Function app site settings.
     """
-    def __init__(self, name: str, auth_settings: AuthSettings, client_cert_mode: FieldMode = None) -> None:
+    def __init__(self, name: str, auth_settings: AuthSettings, site_config: SiteConfig, client_cert_mode: FieldMode = None) -> None:
         super().__init__(AzureResourceType.AZURERM_FUNCTION_APP)
         self.name = name
         self.auth_settings: AuthSettings = auth_settings
+        self.site_config: SiteConfig = site_config
         self.client_cert_mode: FieldMode = client_cert_mode
         self.with_aliases(name)
 
