fade989c676a738e41cc7f1923de2844bd1fb175
Imanuelm <imanuel.m@indeni.com>
2021-06-16 17:50:11 +0300

    modified rest_api resource

--- cloudrail/knowledge/context/aws/apigateway/rest_api_gw.py
+++ cloudrail/knowledge/context/aws/apigateway/rest_api_gw.py
@@ -24,8 +24,9 @@ class RestApiGw(ResourceBasedPolicy):
         api_gw_name: The name of the API gateway.
         api_gateway_type: One of EDGE, REGIONAL, PRIVATE.
         is_public: An indication on if this resource is accessible from the internet.
-        api_gateway_methods: The ApiGatewayMethods associated with this gateway.
+        api_gateway_methods: All the ApiGatewayMethods associated with this gateway.
         api_gw_stages: The stages associated with this REST API Gateway.
+        lambda_allowed_methods: The ApiGatewayMethods associated with this gateway, and are allowed to access a lambda function.
     """
     def __init__(self,
                  rest_api_gw_id: str,
@@ -40,9 +41,9 @@ class RestApiGw(ResourceBasedPolicy):
         self.method_settings: Optional[ApiGatewayMethodSettings] = None
         self.domain: Optional[RestApiGwDomain] = None
         self.api_gw_stages: List[ApiGatewayStage] = []
-
         self.is_public: bool = None
         self.api_gateway_methods: List[ApiGatewayMethod] = []
+        self.lambda_allowed_methods: List[ApiGatewayMethod] = []
 
     def get_keys(self) -> List[str]:
         return [self.rest_api_gw_id]
